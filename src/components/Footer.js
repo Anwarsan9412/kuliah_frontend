49800d2d9972ad50670ede5d2687246fadbb1443
anwar <anwarsanus9412@gmail.com>
2022-03-11 22:46:29 +0700

    commit first

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Copyright @aufaroot18</h2>
+      <p>Website ini dibuat menggunakan ReactJS</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hello = (props) => {
+  // Melakukan destructing props (object)
+  const { name } = props;
+
+  return (
+    <div className="hello">
+      <h2>Hello React</h2>
+      <p>Saya {name} - Frontend Engineer</p>
+    </div>
+  )
+}
+
+export default Hello
